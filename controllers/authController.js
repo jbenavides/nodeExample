fdbd635f21db201f4b189ce222e1d001bd1a4a6a
José Benavides Bermúdez <jbenavides.bermudez@gmail.com>
2018-01-23 17:35:50 -0500

    implementing logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,4 +5,10 @@ exports.login = passport.authenticate('local', {
     failureFlash: 'Failed Login!',
     successRedirect: '/',
     successFlash: 'You are now logged in!'
-});
+})
+
+exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', 'You are now logged out!!')
+    res.redirect('/');
+}
